07c164e28bbfc49f1cfd809d761c36b5d1ec540e
LongDongLeiter <32665128+LongDongLeiter@users.noreply.github.com>
2017-10-10 03:16:19 -0700

    First Commit
    Added Unity Project to GitHub

---
+++ Rick and Morty Game/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour {
+
+
+	public float speed = 1.0f;
+	public GameObject EnemyBullet;
+	Transform firePos2;
+	public GameObject ExplosionGO;
+
+	// Use this for initialization
+	void Start () {
+
+		firePos2 = transform.Find ("firePos2");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (-speed, 0, 0);
+
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			Fire ();
+		}
+	}
+
+	void Fire ()
+	{
+		Instantiate (EnemyBullet, firePos2.position, Quaternion.identity);
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if (other.gameObject.CompareTag ("MORTY BULLET")) {
+
+			PlayExplosion ();
+
+		}
+	}
+
+	void PlayExplosion()
+	{
+		GameObject explosion = (GameObject)Instantiate (ExplosionGO);
+
+		explosion.transform.position = transform.position;
+	}
+}
---
+++ Rick and Morty Game/Assets/Scripts/MortyMover.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortyMover : MonoBehaviour {
+
+
+	public KeyCode moveUp;
+	public KeyCode moveDown;
+	public GameObject Bullet;
+
+	Transform firePos;
+
+	Vector3 tempPos;
+
+	void Start () {
+
+		Vector3 tempPos = transform.position;
+
+
+		firePos = transform.Find ("firePos");
+
+	}
+
+
+	void Update () {
+		//player movement:
+		if (Input.GetKeyUp (moveUp)) {
+			tempPos = transform.position;
+
+			tempPos.x += 0.5f;
+
+			tempPos.y += 1.5f;
+
+			transform.position = tempPos;
+		}
+
+		if (Input.GetKeyDown (moveDown)) {
+			tempPos = transform.position;
+
+			tempPos.x += -0.5f;
+
+			tempPos.y -= 1.5f;
+
+			transform.position = tempPos;
+		}
+
+		//bullet movement:
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			Fire ();
+		}
+
+	}
+
+	void Fire ()
+	{
+		Instantiate (Bullet, firePos.position, Quaternion.identity);
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		//Detect collision of player with enemy or enemy bullet
+
+		if (other.gameObject.CompareTag ("ENEMY") || other.gameObject.CompareTag("ENEMY BULLET")) {
+
+			Destroy(gameObject);
+			Destroy (other.gameObject);
+		}
+		
+	}
+}
